cc5b0f19fc52cca8355fe8e5acfd943403f57ae9
daifuming <daifuming123@gmail.com>
2019-03-13 08:55:04 +0800

    first commit

---
+++ res/assets_gen.bat
@@ -0,0 +1,32 @@
+@rem ---------------------------------------------------------------------------------------------------
+@rem һ��˫���������⡢�ַ��������塢ͼƬ��UI�ȶ������ļ�(���е���assets_gen.py)                                   
+@rem �����޸� AWTK_BIN_DIR=E:/zlgopen/awtk/bin Ϊ����awtk��Ӧ��binĿ¼
+@rem ����assets_gen.bat˵��
+@rem	  1 Ϊ��(ʹ��800*480��Ļ)��
+@rem		��assets_gen.bat
+@rem	  2 -480_272(ʹ��480*272��Ļ):
+@rem		��assets_gen.bat -480_272	
+@rem �ġ�%PY_VERSION% assets_gen.py %AWTK_BIN_DIR%  ./src/assets/raw ./src/assets/inc˵����
+@rem    1 Ϊ�գ�
+@rem      ��assets_gen.py AWTK_BIN_DIR ./src/assets/raw ./src/assets/inc���������⡢�ַ��������塢ͼƬ��UI�ȶ������ļ�
+@rem    2 -style 
+@rem      ��assets_gen.py AWTK_BIN_DIR ./src/assets/raw ./src/assets/inc -style��  ��������
+@rem    3 -string
+@rem      ��assets_gen.py AWTK_BIN_DIR ./src/assets/raw ./src/assets/inc -string�� �����ַ���
+@rem    4 -font
+@rem      ��assets_gen.py AWTK_BIN_DIR ./src/assets/raw ./src/assets/inc -font��   ��������
+@rem    5 -image
+@rem      ��assets_gen.py AWTK_BIN_DIR ./src/assets/raw ./src/assets/inc -image��  ����ͼƬ
+@rem    6 -ui
+@rem      ��assets_gen.py AWTK_BIN_DIR ./src/assets/raw ./src/assets/inc -ui��     ����ui�ļ�
+	
+@rem ----------------------------------------------------------------------------------------------------
+
+@set AWTK_BIN_DIR=%1%
+@set PY_VERSION=python
+::@if exist %SystemRoot%\py.exe (
+::	@set PY_VERSION=py -2
+::)
+
+%PY_VERSION% ../assets_gen.py %AWTK_BIN_DIR% ./assets/raw ./assets/inc
+%PY_VERSION% ../assets_c_gen.py ./assets
---
+++ src/window_main.c
@@ -0,0 +1,96 @@
+﻿/**
+ * File:   edit.c
+ * Author: AWTK Develop Team
+ * Brief:  window main
+ *
+ * Copyright (c) 2018 - 2018  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-11-20 TangJunJie created
+ *
+ */
+#include "awtk.h"
+
+
+/**
+ * 点击application按钮，创建application窗口
+ * */
+static ret_t on_open_application(void* ctx, event_t* e){
+  printf("click application\n");
+  (void)e;
+  (void)ctx;
+  widget_t* win = window_open("application");
+  if (win){
+    // 初始化界面控件
+    return RET_OK;
+  }
+
+  return RET_FAIL;
+}
+
+/**
+ * 点击weather按钮，创建weather窗口
+ * */
+static ret_t on_open_weather(void* ctx, event_t* e){
+  return RET_FAIL;
+}
+
+/**
+ * 点击health按钮，创建health窗口
+ * */
+static ret_t on_open_health(void* ctx, event_t* e){
+  return RET_FAIL;
+}
+
+/**
+ * 为一个控件添加点击事件
+ * */
+static ret_t install_one(void* ctx, const void* iter)
+{
+  widget_t* widget = WIDGET(iter);
+
+  if (widget->name != NULL){
+    const char* name = widget->name;
+    if (strstr(name, "application") != NULL){
+      widget_on(widget, EVT_CLICK, on_open_application, widget);
+    }else if(strstr(name, "weather") != NULL){
+      widget_on(widget, EVT_CLICK, on_open_weather, widget);
+    }else if (strstr(name, "health") != NULL){
+      widget_on(widget, EVT_CLICK, on_open_health, widget);
+    }
+  }
+
+  return RET_OK;
+}
+
+
+/**
+ * 为widget所有控件添加点击事件
+ * */
+static void install_click_hander(widget_t *widget)
+{
+  widget_foreach(widget, install_one, widget);
+}  
+
+
+/**
+ * 初始化
+ */
+void application_init() {
+  widget_t* win = window_open("main");
+  if (win){
+    widget_layout(win);
+
+    // 添加点击事件
+    install_click_hander(win);
+  }
+}
